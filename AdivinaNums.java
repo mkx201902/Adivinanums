d3f20f3f82db119cf556d3b0b8871721517f35ab
mkx201902 <59641569+mkx201902@users.noreply.github.com>
2020-01-13 08:54:12 +0100

    el primer avance...
    8:53 13.01.2020

---
+++ AdivinaNums.java
@@ -0,0 +1,35 @@
+package paq;
+import java.util.Scanner;
+class AdivinaNums {
+	public static void main(String[] args) {
+	//declaro variable numerica acierto y variable boolean aciertovalido	
+	int acierto, segundos;
+	boolean aciertoValido, segundosV;
+	//inicializo variable numerica
+	acierto=0;
+	segundos=0;
+	
+	
+	//invoco el método de obtención de variable por teclado
+	Scanner teclado = new Scanner( System.in );
+	//invoco un bucle de obtención de número positivo para la variable acierto
+	do	{
+		System.out.println("Introduce el número de aciertos");
+		acierto = teclado.nextInt();
+		aciertoValido = acierto>0;
+				if (!aciertoValido)
+			System.out.println("Error, intentalo otra vez\n");
+		}while (!aciertoValido); //fin repetir-mientras
+	do	{
+		System.out.println("¿En cuánto tiempo(en segundos)?");
+		segundos = teclado.nextInt();
+		segundosV=segundos>0;
+				if (!segundosV)
+			System.out.println("Error, intentalo otra vez\n");
+		}while (!segundosV); //fin repetir-mientras
+	
+		
+
+	}
+
+}
